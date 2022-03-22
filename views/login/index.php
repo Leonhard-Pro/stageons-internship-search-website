ecf3de439abe9c3885edf83ea4a6c0598f6148e7
Criy0s <jordan.guerreiro@viacesi.fr>
2022-03-22 16:15:49 +0100

    style.scss || cookies_banner.scss || index.php
    Modification of the html architecture on index.php
    Fixed a bug with blur on cookies_banner.scss
    Implemented responsive black cesi logo with text on styles.scss

--- views/login/index.php
+++ views/login/index.php
@@ -41,29 +41,26 @@
             </div>
 
         </div>
-</form>
-        <?php if (!isset($_COOKIE['Cookies']) || $_COOKIE['Cookies'] == false): ?>
-            <div class="blurry_background-cookies">
-
-            </div>
-            <div class="cookie-handband">
-            <div class="cookie-informations">
-                <div class="cookie-tag-management">
+    </form>
+<?php if (!isset($_COOKIE['Cookies']) || $_COOKIE['Cookies'] == false): ?>
+    <div class="blurry_background-cookies"></div>
+    <div class="cookie-handband">
+        <div class="cookie-informations">
+            <div class="cookie-tag-management">
                 <img src="views/resources/logo_cesi_with_text.svg" alt="Logo Cesi" id="logo_cesi-cookies">
                 <div class="cookie-title">THIS SITE USES COOKIES</div>
                 <div class="cookies-warning">
                 We use cookies to personalize content and advertisements, to provide social media features and to analyze our traffic. We also share information about your use of our site with our social media, advertising and analytics partners who may combine it with other information that you have provided to them or that they have collected from your use of their services. You consent to our cookies if you continue to use our website.
                 </div>
-                </div>
-
-                <div class="cookie-choose">
-                    <form action="" method="POST">
-                        <button class="button-cookies" type="submit" name="accept-cookies" value="">Accept</button>
-                    </form>
-                </div>
             </div>
+            <div class="cookie-choose">
+                <form action="" method="POST">
+                    <button class="button-cookies" type="submit" name="accept-cookies" value="">Accept</button>
+                </form>
             </div>
-        <?php endif; ?>
+        </div>
+    </div>
+<?php endif; ?>
     
 </body>
 

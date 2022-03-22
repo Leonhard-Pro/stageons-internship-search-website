d51b35f104aaf407ad155b665b5cdb28006c2024
kevinroux69 <kevin.roux.69.bbox@gmail.com>
2022-03-22 09:21:26 +0100

    MainStyle.scss || myaccount.php
    Added myaccount GUI (added header in php)

---
+++ Documents Html/myaccount.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Stageons ~ myaccount</title>
+    <link rel="stylesheet" href="../Feuilles de styles/MainStyle.css">
+</head>
+
+<?php
+include("header.html");
+?>
+
+<body>
+    <div id="My_account">
+
+        <h3>Personnal information</h3><br><br>
+        <input type="email" id="Email_pro" class="input" placeholder="Professional Email"><br><br>
+        <input type="password" id="Password" class="input" placeholder="Password"><br><br>
+        <input type="text" id="First_name" class="input" placeholder="First name"><br><br>
+        <input type="text" id="Surname" class="input" placeholder="Surname"><br><br>
+        <input type="text" id="Location" class="input" placeholder="Location"><br><br>
+        <input type="email" id="Email_per" class="input" placeholder="Personnal email"><br><br>
+        <input type="button" class="button" value="Logout">
+
+    </div>
+</body>
+
+</html>

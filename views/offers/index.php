c206d0d77a676bf4dce1906ca6df2ad69ff4f03b
Charles <samama.charles@gmail.com>
2022-03-22 16:05:21 +0100

    Views/offers/index.php
    Heavy modifications for the responsiveness of the offer page

--- views/offers/index.php
+++ views/offers/index.php
@@ -9,40 +9,41 @@
     <script src="views/js/filter.js" async></script>
 </head>
 
+
+<body>
 <?php
 include("Documents Html/header.html");
 ?>
-
-<body>
-    <div id="Filtre_1" class="Filtre">
-        <input type="text" id="What" placeholder="What :">
-        <input type="text" id="Where" placeholder="Where :">
-        <div id="bouton">
-            <input type="button" id="Search" value="Search">
+    <div id="Filtres">
+        <div id="Filtre_1" class="Filtre">
+            <input type="text" id="What" placeholder="What :">
+            <input type="text" id="Where" placeholder="Where :">
+            <div id="bouton">
+                <input type="button" id="Search" value="Search">
+            </div>
+            <div id=Icon_filtre onclick="Show_all()"></div>
+            <div id=Icon_barre onclick="Show_all()"></div>
         </div>
-        <div id=Icon_filtre onclick="Show_all()"></div>
-        <div id=Icon_barre onclick="Show_all()"></div>
-    </div>
-    <div id="Filtre_2" class="Filtre">
-        <input type="text" id="Skills" placeholder="Skills :">
-        <input type="text" id="Date" placeholder="Date :">
-        <input type="text" id="Companies" placeholder="Companies :">
-        <input type="text" id="Duration" placeholder="Duration :">
-        <br><br>
-    </div>
-    <div id="Filtre_3" class="Filtre">
-        <input type="text" id="Class_concerned" placeholder="Class concerned :">
-        <input type="text" id="Remuneration" placeholder="Remuneration :">
-        <input type="text" id="Number_places" placeholder="Number places :">
+        <div id="Filtre_2" class="Filtre">
+            <input type="text" id="Skills" placeholder="Skills :">
+            <input type="text" id="Date" placeholder="Date of beginning :">
+            <input type="text" id="Companies" placeholder="Companies :">
+            <input type="text" id="Duration" placeholder="Duration :">
+            <br><br>
+        </div>
+        <div id="Filtre_3" class="Filtre">
+            <input type="text" id="Class_concerned" placeholder="Concerned classes :">
+            <input type="text" id="Remuneration" placeholder="Remuneration :">
+            <input type="text" id="Number_places" placeholder="Number of places :">
 
+        </div>
     </div>
-
     <div class="list">
         <?php
         for ($i=0; $i<10; $i++)
         {
             echo("<div class=\"tab_list\">");
-            echo("<img src=\"views/resources/icon_google.png\" alt=\"icon\" class=\"icon_tab_list\">");
+            //echo("<img src=\"views/resources/icon_google.png\" alt=\"icon\" class=\"icon_tab_list\">");
             echo("<div>");
             echo("<h3>Title of the offer</h3>");
             echo("<p>Google, one of the most famous companies in the world, is known for being the biggest IT company in terms of use per day. Bla bla bla, this is a test verse. We continue, as we want to test responsiveness and any kind of automatic modification.</p>");

f4ffcc2335ea3ae861f966fc7b184b1b4617b76e
Criy0s <jordan.guerreiro@viacesi.fr>
2022-03-23 14:01:36 +0100

    SessionStart.php || SessionStop.php
    Add a variable $userType for all our dynamic pages in SessionStart.php
    
    Add condition in SessionStop.php to destroy
    the session and to load the login page

--- models/SessionStart.php
+++ models/SessionStart.php
@@ -4,5 +4,8 @@ require("models/CL_Class_Pilot.php");
 require("models/CL_Delegate.php");
 require("models/CL_Student.php");
 session_start();
-if(isset($_SESSION['user'])) $user = $_SESSION['user'];
+if(isset($_SESSION['user'])) {
+    $user = $_SESSION['user'];
+    $userType = $user->getUserType();
+}
 ?>

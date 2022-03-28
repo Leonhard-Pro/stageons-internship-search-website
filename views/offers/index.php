0b9962b023b6a47e7c1806a14f79dac706333dd4
CharlesSamamaRochegude <charles.samamarochegude@viacesi.fr>
2022-03-28 15:36:14 +0200

    Views/offers
    Interactivity with the offer list

--- views/offers/index.php
+++ views/offers/index.php
@@ -1,20 +1,16 @@
 <div id="display_list">
-    <div class="list">
+    <div class="list" id="left_pannel">
         <?php
         for ($i = 0; $i < 10; $i++) {
-            echo ("<div class=\"tab_list\">");
-            echo ("<div>");
-            echo ("<h3>Title of the offer $i</h3>");
-            echo ("<p>Google, one of the most famous companies in the world, is known for being the biggest IT company in terms of use per day. Bla bla bla, this is a test verse. We continue, as we want to test responsiveness and any kind of automatic modification.</p>");
-            echo ("</div>");
-            echo ("</div>");
+            echo ('<div class="tab_list" onclick="PannelAppear()">
+                    <div>
+                        <h3>Title of the offer '.$i.'</h3>
+                        <p>Google, one of the most famous companies in the world, is known for being the biggest IT company in terms of use per day. Bla bla bla, this is a test verse. We continue, as we want to test responsiveness and any kind of automatic modification.</p>
+                    </div>
+                </div>');
         }
         ?>
     </div>
-    <div class="container_info">
-        <div class="further_info">
-            <h2>Title of the offer</h2>
-            <p>Google, one of the most famous companies in the world, is known for being the biggest IT company in terms of use per day. Bla bla bla, this is a test verse. We continue, as we want to test responsiveness and any kind of automatic modification.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam erat odio, laoreet in velit ut, posuere egestas diam. Vivamus mauris lacus, imperdiet in sem eget, consequat varius sem. Proin commodo sollicitudin ex, sed fermentum ligula scelerisque eu. Phasellus luctus mauris egestas nisl eleifend, id vulputate justo maximus. Nullam efficitur urna eu felis ullamcorper, at suscipit arcu dictum. Nam mauris sapien, sodales ut tellus vel, convallis ullamcorper ante. Mauris pretium pretium tempus. Etiam commodo eros augue, pulvinar lacinia nunc malesuada vitae. Quisque ultricies mattis urna quis tincidunt. Ut vel ligula aliquet, faucibus enim at, dictum quam. Nullam massa augue, consectetur non vehicula vitae, sollicitudin sed felis. Proin pharetra rutrum lorem, quis aliquam ex pellentesque at. Vestibulum elementum magna sed justo blandit, sit amet eleifend metus cursus. Etiam volutpat vel nisl sit amet cursus.  </p>
-        </div>
+    <div class="container_info" id="right_pannel">
     </div>
 </div>

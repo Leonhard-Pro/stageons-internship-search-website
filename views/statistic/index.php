3100f6b71e2c555222128b580abc6fee84ecd408
kevinroux69 <kevin.roux.69.bbox@gmail.com>
2022-03-28 14:55:00 +0200

    Statistic
    Add the first version of graph

---
+++ views/statistic/index.php
@@ -0,0 +1,13 @@
+ <!-- https://www.chartjs.org/docs/latest/ -->
+
+
+ <body>
+     <div id="doughnut">
+         <canvas id="graph" width="400%" height="100%"></canvas>
+     </div>
+
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.1/chart.min.js"></script>
+
+     <script src="views\statistic\cheese.js"></script>
+
+ </body>

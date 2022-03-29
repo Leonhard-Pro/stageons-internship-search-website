6a962cfdfd4926197664de3e3be2dbd554ad55b8
kevinroux69 <kevin.roux.69.bbox@gmail.com>
2022-03-29 15:29:55 +0200

    Advancement
    Add GUI

---
+++ views/advancement/index.php
@@ -0,0 +1,17 @@
+<div id="main">
+    <div class="menu-advancement">
+        <form action="" method="POST">
+            <input type="submit" name="typeAdvancement" value="Wishlist">
+            <input type="submit" name="typeAdvancement" value="Applied">
+            <input type="submit" name="typeAdvancement" value="Step 1">
+            <input type="submit" name="typeAdvancement" value="Step 2">
+            <input type="submit" name="typeAdvancement" value="Step 3">
+            <input type="submit" name="typeAdvancement" value="Step 4">
+            <input type="submit" name="typeAdvancement" value="Step 5">
+            <input type="submit" name="typeAdvancement" value="Step 6">
+        </form>
+    </div>
+
+    <div id="filters">
+        <?php include("views/layout/filters.php"); ?>
+    </div>
